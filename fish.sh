03f5be06dd1b2f27d721663400718c12b2162b95
JulieFf7 <102351065+JulieFf7@users.noreply.github.com>
2022-04-03 20:47:14 +0200

    Bug fixes

--- fish.sh
+++ fish.sh
@@ -28,13 +28,15 @@ yesfish || error "Exiting"
 
 
 installfish(){ \
-  sudo dnf install fish
+  sudo dnf install fish util-linux-user 
 
   }
 installfish || error "Failed installing fish"
 
 setfish(){ \
-  echo /usr/bin/fish | sudo tee -a /etc/shells && chsh -s /usr/bin/fish
+  echo /usr/bin/fish | sudo tee -a /etc/shells
+
+   chsh -s /usr/bin/fish
 
   }
 setfish || error "Failed to set fish as default shell"
